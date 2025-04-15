8483e0079270fe5cd30698df2b04b649ec8292cf
zang <2556450572@qq.com>
2025-04-15 19:58:32 +0800

    feature : 开启x-file-storage配置

--- AgentX/src/main/java/org/xhy/AgentXApplication.java
+++ AgentX/src/main/java/org/xhy/AgentXApplication.java
@@ -1,5 +1,6 @@
 package org.xhy;
 
+import org.dromara.x.file.storage.spring.EnableFileStorage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 应用入口类
  */
 @SpringBootApplication
+@EnableFileStorage
 public class AgentXApplication {
 
     public static void main(String[] args) {
