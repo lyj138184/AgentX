cddb1604b2b66a5c16f3ff3497694b10575e4d12
Xhy <xhyovo@qq.com>
2025-04-30 14:36:56 +0800

    feat(user):用户领域
    - 用户登录注册(WIP)
    - 获取用户信息
    
    # Conflicts:
    #	AgentX/src/main/java/org/xhy/infrastructure/entity/BaseEntity.java
    #	AgentX/src/main/java/org/xhy/interfaces/api/common/Result.java
    #	AgentX/src/main/java/org/xhy/interfaces/dto/ChatRequest.java

---
+++ AgentX/src/main/java/org/xhy/application/user/service/LoginAppService.java
@@ -0,0 +1,29 @@
+package org.xhy.application.user.service;
+
+import org.springframework.stereotype.Service;
+import org.xhy.application.user.assembler.UserAssembler;
+import org.xhy.domain.user.model.UserEntity;
+import org.xhy.domain.user.service.UserDomainService;
+import org.xhy.infrastructure.utils.JwtUtils;
+import org.xhy.interfaces.dto.user.request.LoginRequest;
+import org.xhy.interfaces.dto.user.request.RegisterRequest;
+
+@Service
+public class LoginAppService {
+
+    private final UserDomainService userDomainService;
+
+    public LoginAppService(UserDomainService userDomainService) {
+        this.userDomainService = userDomainService;
+    }
+
+    public String login(LoginRequest loginRequest) {
+        UserEntity userEntity = userDomainService.login(loginRequest.getAccount(), loginRequest.getPassword());
+        return JwtUtils.generateToken(userEntity.getId());
+    }
+
+
+    public void register(RegisterRequest registerRequest){
+        userDomainService.register(registerRequest.getEmail(), registerRequest.getPhone(), registerRequest.getPassword());
+    }
+}
---
+++ AgentX/src/test/java/org/xhy/domain/user/service/UserDomainServiceTest.java
@@ -0,0 +1,13 @@
+package org.xhy.domain.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserDomainServiceTest {
+
+    @Test
+    public void test() {
+        System.out.println(1);
+    }
+}
